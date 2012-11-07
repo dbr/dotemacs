1170bdaa42fb10e84c4302376b50c38b4022917f
dbr <dbr.onix@gmail.com>
2012-11-07 22:58:56 +1030

    Use builtin org-mode from emacs24
    Emacs 23 contained an old verison of org-mode before org-babel

--- init.el
+++ init.el
@@ -1,16 +1,22 @@
-;; Loads the config.org babel file
+;; Loads the config.org babel file, based on
 ;; http://orgmode.org/worg/org-contrib/babel/intro.html
 
+
+;; Determine the directory containing this file
 (setq dotfiles-dir (file-name-directory (or load-file-name (buffer-file-name))))
 
-(add-to-list 'load-path (expand-file-name
-                         "lisp" (expand-file-name
-                                 "org-mode" (expand-file-name
-                                        "ext" dotfiles-dir))))
-;; Load up Org Mode and Babel
+;; emacs 23 requires org-mode checkout from ext/org-mode/
+;; (org-mode version 7.5 definitely works)
+(if (< emacs-major-version 24)
+    (add-to-list 'load-path (expand-file-name
+                             "lisp" (expand-file-name
+                                     "org-mode" (expand-file-name
+                                                 "ext" dotfiles-dir)))))
+
+;; Load up org-mode and org-babel
 (require 'org-install)
 (require 'ob-tangle)
 
-;; load up the main file
+;; Load up the main file
 (org-babel-load-file (expand-file-name "config.org" dotfiles-dir))
 
