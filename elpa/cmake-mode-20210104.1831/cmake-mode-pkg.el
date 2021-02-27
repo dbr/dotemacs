ca52edcb4aa4242417bc2a1481c8bdb35750ab89
dbr <dbr.onix@gmail.com>
2021-02-28 09:49:42 +1100

    Much-newer version of cmake-mode

---
+++ elpa/cmake-mode-20210104.1831/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20210104.1831" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "ea3f09aa401b08a2977c9fec61711f48bba856e7")
