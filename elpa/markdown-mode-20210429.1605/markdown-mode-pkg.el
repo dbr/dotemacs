b456662220518f5d16d476fdff26d794de32b46b
dbr <dbr.onix@gmail.com>
2021-05-03 11:32:58 +1000

    Add lsp-mode for rust analyzer

---
+++ elpa/markdown-mode-20210429.1605/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20210429.1605" "Major mode for Markdown-formatted text" '((emacs "25.1")) :commit "94c65e2de2e10b7f3a5e72d412c64ab83b2b1a5e" :authors '(("Jason R. Blevins" . "jblevins@xbeta.org")) :maintainer '("Jason R. Blevins" . "jblevins@xbeta.org") :keywords '("markdown" "github flavored markdown" "itex") :url "https://jblevins.org/projects/markdown-mode/")
