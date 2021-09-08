284bbab3eef6e571039afb26dfe9e4dafe9f125d
dbr <dbr.onix@gmail.com>
2021-09-08 15:50:31 +1000

    Update thirdparty stuff

---
+++ elpa/magit-section-20210829.1849/magit-section-pkg.el
@@ -0,0 +1,13 @@
+(define-package "magit-section" "20210829.1849" "Sections for read-only buffers"
+  '((emacs "25.1")
+    (dash "2.18.1"))
+  :commit "2fe42f63d558e488ad71d050e8ca5e8565c845ae" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
