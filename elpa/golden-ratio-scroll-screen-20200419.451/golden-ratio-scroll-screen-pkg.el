a48f0716f71749b955164493c2abb5a607648d70
dbr <dbr.onix@gmail.com>
2021-11-25 13:36:15 +1100

    Less jarring scroll behaviour

---
+++ elpa/golden-ratio-scroll-screen-20200419.451/golden-ratio-scroll-screen-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from golden-ratio-scroll-screen-20200419.451/golden-ratio-scroll-screen.el  -*- no-byte-compile: t -*-
+(define-package "golden-ratio-scroll-screen" "20200419.451" "Scroll half screen down or up, and highlight current line" 'nil :commit "1b6ff0e3e8822423335d3f7d88c1fcb4cf43ce42" :authors '(("纪秀峰 <jixiuf at gmail dot com>")) :maintainer '("纪秀峰 <jixiuf at gmail dot com>") :keywords '("scroll" "screen" "highlight") :url "https://github.com/jixiuf/golden-ratio-scroll-screen")
